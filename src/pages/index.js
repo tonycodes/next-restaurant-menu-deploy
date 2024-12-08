e6b3bbd56f091d4b2958f614ae1644124f9f0ce2
Tony James <me@tony.codes>
2024-12-08 11:37:56 +0000

    Add main page component

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import MenuBuilder from '../components/MenuBuilder';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <MenuBuilder />
+    </div>
+  );
+}
